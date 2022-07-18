cabe38d33fd330c8172940c35e3ddb9f59ec4e51
rodrigonexter <rodrigoufvjmsi@gmail.com>
2022-07-18 16:29:43 -0300

    feat: Teachers View

--- pages/teachers.vue
+++ pages/teachers.vue
@@ -1,10 +1,266 @@
+<!-- eslint-disable no-undef -->
 <template>
-  <h1>professores</h1>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          sort-by="name"
+          class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Professores Orientadores</v-toolbar-title>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="dialog" max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    Novo
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                  </v-card-title>
+                  <!-- name, email, phone, teacher_id -->
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="6">
+                          <v-text-field
+                            v-model="editedItem.name"
+                            label="Nome do Professor"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                          <v-text-field
+                            v-model="editedItem.email"
+                            label="E-mail"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="6">
+                          <v-text-field
+                            v-model="editedItem.phone"
+                            label="Telefone"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="6">
+                          <v-text-field
+                            v-model="editedItem.teacher_id"
+                            label="Código"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="close">
+                      Cancelar
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="save">
+                      Salvar
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDelete" max-width="600px">
+                <v-card>
+                  <v-card-title class="text-h5"
+                    >Tem certeza que deseja apagar este item?</v-card-title
+                  >
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete"
+                      >Cancelar</v-btn
+                    >
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                      >OK</v-btn
+                    >
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:[`item.actions`]="{ item }">
+            <v-icon small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+          </template>
+
+          <template v-slot:no-data>
+            <v-btn color="primary" @click="initialize"> Reset </v-btn>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
-</script>
+import axios from 'axios'
 
-<style>
-</style>
+export default {
+  data: () => ({
+    dialog: false,
+    dialogDelete: false,
+    headers: [
+      // name, email, phone, teacher_id
+
+      {
+        text: 'Nome',
+        align: 'start',
+        sortable: false,
+        value: 'name',
+      },
+      { text: 'Código', value: 'teacher_id', sortable: false },
+      { text: 'Email', value: 'email', sortable: false },
+      { text: 'Telefone', value: 'phone', sortable: false },
+      { text: 'Ações', value: 'actions', sortable: false },
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      id: '',
+      name: '',
+      teacher_id: '',
+      email: '',
+      phone: '',
+    },
+    defaultItem: {
+      id: '',
+      name: '',
+      teacher_id: '',
+      email: '',
+      phone: '',
+    },
+  }),
+
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'Novo Professor' : 'Editar Professor'
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
+
+  created() {
+    this.initialize()
+  },
+
+  methods: {
+    async store() {
+      try {
+        // name, email, phone, teacher_id
+        const student = await axios.post(`http://127.0.0.1:3333/teachers`, {
+          name: this.editedItem.name,
+          email: this.editedItem.email,
+          phone: this.editedItem.phone,
+          teacher_id: this.editedItem.teacher_id,
+        })
+
+        // eslint-disable-next-line no-undef
+        console.log(student)
+        this.initialize()
+      } catch (error) {
+        // eslint-disable-next-line no-undef
+        console.log(error)
+      }
+    },
+
+    async update(id) {
+      try {
+        const student = await axios.put(
+          `http://127.0.0.1:3333/teachers/${id}`,
+          this.editedItem
+        )
+
+        // eslint-disable-next-line no-undef
+        console.log(student)
+        this.initialize()
+      } catch (error) {
+        // eslint-disable-next-line no-undef
+        console.error(error)
+      }
+    },
+    async destroy(id) {
+      await axios.delete(`http://127.0.0.1:3333/teachers/${id}`)
+      this.initialize()
+    },
+
+    async initialize() {
+      const students = await axios.get(`http://127.0.0.1:3333/teachers`)
+
+      this.desserts = students.data
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm() {
+      this.desserts.splice(this.editedIndex, 1)
+      this.destroy(this.editedItem.id)
+      // eslint-disable-next-line no-undef
+      console.log(this.editedIndex)
+      this.closeDelete()
+    },
+
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        // Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        this.update(this.editedItem.id)
+        // eslint-disable-next-line no-undef
+        console.log(this.editedItem)
+      } else {
+        this.store()
+      }
+      this.close()
+    },
+  },
+}
+</script>
